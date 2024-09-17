299c768f8feba9a2931b30b20d9f18706a1331c2
javapak24 <ajava2424@gmail.com>
2024-09-16 20:27:51 -0400

    fix user favmovies

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -11,7 +11,6 @@ import { Router } from '@angular/router';
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent implements OnInit {
-  movies: any[]= [];
   userData: any = {};
   favoriteMovies: any[] = [];
   constructor(
@@ -22,21 +21,22 @@ export class UserProfileComponent implements OnInit {
     { this.userData = JSON.parse(localStorage.getItem("user") || ""); }
 
  ngOnInit(): void {
-  this.getUser();
+  // this.getUser();
+  this.getFavMovies();
  }
 
- getUser(): void {
-  this.fetchApiData.getUser().subscribe((resp: any) => {
-    this.userData = {
-      ...resp,
-      id: resp._id,
-      password: this.userData.password,
-      token: this.userData.token
-    };
-    localStorage.setItem("user", JSON.stringify(this.userData));
-    console.log(this.userData);
-  });
-}
+//  getUser(): void {
+//   this.fetchApiData.getUser().subscribe((resp: any) => {
+//     this.userData = {
+//       ...resp,
+//       id: resp._id,
+//       password: this.userData.password,
+//       token: this.userData.token
+//     };
+//     localStorage.setItem("user", JSON.stringify(this.userData));
+//     console.log(this.userData);
+//   });
+// }
 
 editUser(): void{
   this.fetchApiData.editUser(this.userData.Username, this.userData).subscribe((resp: any) => {
@@ -51,12 +51,12 @@ editUser(): void{
   });
 } 
 
-getUserFavoriteMovies(): void{
-  this.fetchApiData.getUserFavoriteMovies().subscribe((resp : any) => {
-    this.userData.favoriteMovies = resp.FavoriteMovies;
-    return this.userData.favoriteMovies;
-})
-}
+// getUserFavoriteMovies(): void{
+//   this.fetchApiData.getUserFavoriteMovies().subscribe((resp : any) => {
+//     this.userData.favoriteMovies = resp.FavoriteMovies;
+//     return this.userData.favoriteMovies;
+// })
+// }
 
 showGenreAlert(genre: any): void {
   alert(genre);
@@ -70,11 +70,13 @@ showSynopsisAlert(synopsis: any): void {
   alert(synopsis);
 }
 
-getMovies(): void {
+getFavMovies(): void {
   this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-    this.movies = resp;
-    console.log(this.movies);
-    return this.movies;
+    this.favoriteMovies = resp.filter((m : any) => {
+      return this.userData.FavoriteMovies.includes(m._id);
+    });
+    console.log(this.favoriteMovies);
+    return this.favoriteMovies;
   });
 }
 
@@ -87,40 +89,40 @@ deleteUser(): void{
 }
 
 // TODO: Implement favorite button
-addToFavorites(movieTitle: string): void {
-  const user: any = JSON.parse(localStorage.getItem('user') as any);
-  this.fetchApiData
-    .addFavoriteMovie(user.Username, movieTitle)
-    .subscribe((res: any) => {
-      console.log(res);
-      this.getMovies();
-    });
-    this.snackBar.open("movie added to favorites", 'OK', {
-      duration: 2000
-   });
-}
+// addToFavorites(movieTitle: string): void {
+//   const user: any = JSON.parse(localStorage.getItem('user') as any);
+//   this.fetchApiData
+//     .addFavoriteMovie(user.Username, movieTitle)
+//     .subscribe((res: any) => {
+//       console.log(res);
+//       this.getMovies();
+//     });
+//     this.snackBar.open("movie added to favorites", 'OK', {
+//       duration: 2000
+//    });
+// }
 
 // TODO: Implement delete button
-removeFromFavorites(movieTitle: string): void {
-  const user: any = JSON.parse(localStorage.getItem('user') as any);
-  this.fetchApiData
-    .deleteFavoriteMovie(user.Username, movieTitle)
-    .subscribe((res: any) => {
-      console.log(res);
-      this.getMovies();
-    });
-    this.snackBar.open("movie removed from favorites", 'OK', {
-      duration: 2000
-   });
-}
+// removeFromFavorites(movieTitle: string): void {
+//   const user: any = JSON.parse(localStorage.getItem('user') as any);
+//   this.fetchApiData
+//     .deleteFavoriteMovie(user.Username, movieTitle)
+//     .subscribe((res: any) => {
+//       console.log(res);
+//       this.getMovies();
+//     });
+//     this.snackBar.open("movie removed from favorites", 'OK', {
+//       duration: 2000
+//    });
+// }
 
-isFavorite(movieId: string): boolean {
-  const userFavorites: any = JSON.parse(
-    localStorage.getItem('user') as any
-  ).FavoriteMovies;
-  console.log(userFavorites);
-  return userFavorites.includes(movieId);
-}
+// isFavorite(movieId: string): boolean {
+//   const userFavorites: any = JSON.parse(
+//     localStorage.getItem('user') as any
+//   ).FavoriteMovies;
+//   console.log(userFavorites);
+//   return userFavorites.includes(movieId);
+// }
 
 
 logoutUser(): void{
